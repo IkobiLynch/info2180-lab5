3d0807c2d233ca0706085140403e89ddd719a8d6
IkobiLynch <82119066+IkobiLynch@users.noreply.github.com>
2022-11-14 21:36:01 -0500

    Completed excercise 3

---
+++ world.js
@@ -0,0 +1,31 @@
+"use strict";
+
+window.onload = function(){
+    var btn = document.getElementById('lookup');
+    var formInput = document.getElementById('country');
+    var result = document.getElementById('result');
+
+    var phpURL = 'http://localhost/info2180-lab5/world.php?country=';
+
+    btn.addEventListener('click', function(event){
+        event.preventDefault();
+        let query = formInput.value;
+        let request = new URL(phpURL + query);
+
+        fetch (request)
+            .then(response => {
+                if(response.ok){
+                    return response.text()
+                }
+                else{
+                    return Promise.reject('something went wrong')
+                }
+            })
+            .then (function (data){
+                result.innerHTML = data;
+            })
+            .catch (error => console.log('There was an error: ' + error))
+
+
+    })
+}
